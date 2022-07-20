a3d221314a80971a8c0280d55c6e11b1e81edfc5
Thien Tam Ho <thientam.ho@vn.wilmar-intl.com>
2022-07-20 21:53:13 +0700

    add regex for form input

--- lib/src/screens/login/cubit/login_cubit.dart
+++ lib/src/screens/login/cubit/login_cubit.dart
@@ -11,12 +11,27 @@ part 'login_state.dart';
 class LoginCubit extends Cubit<LoginState> with BaseBloc {
   LoginCubit(this._repository) : super(const LoginState(username: InputModel(), password: InputModel()));
   final AuthenticationRepository _repository;
-  void usernameChanged(InputModel value) {
-    emit(state.copyWith(username: value));
+  void usernameChanged(InputModel input) {
+    final usernameRegex = RegExp(r'^[a-zA-Z0-9_-]{8,25}$');
+    if (usernameRegex.hasMatch(input.value)) {
+      input.copyWith(isValid: true);
+    } else {
+      input.copyWith(isValid: false, error: 'Username must be between 8 to 25 characters');
+    }
+    emit(state.copyWith(username: input));
   }
 
-  void passwordChanged(InputModel value) {
-    emit(state.copyWith(password: value));
+  void passwordChanged(InputModel input) {
+    final passwordRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$');
+    if (passwordRegex.hasMatch(input.value)) {
+      input.copyWith(isValid: true);
+    } else {
+      input.copyWith(
+          isValid: false,
+          error: 'Password must be minimum eight characters, at least one upper case English letter,'
+              'one lower case English letter, one number and one special character');
+    }
+    emit(state.copyWith(password: input));
   }
 
   void login() async {
